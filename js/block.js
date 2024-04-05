664a72df0792c5b85b9fc07e7d3402a06e408feb
Roland Schellhorn <hello@pixelparker.com>
2024-04-05 10:42:13 +0200

    update blockheight

--- js/block.js
+++ js/block.js
@@ -3,7 +3,7 @@
  * @version 1.0
  */
 
-const halvingBlock = 837784 // 84000
+const halvingBlock = 840000 // 84000
 const updateInterval = 21000;
 const defaultTimeBetweenBlocks = 10;
 let lastBlock = false;
